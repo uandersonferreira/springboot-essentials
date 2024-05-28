788670700b9c35181e62d2f246574150d0af43cf
Uanderson Ferreira de Oliveira <uanderson.oliveira@estudante.ifto.edu.br>
2024-05-27 23:57:01 -0300

    adicionando o postgresql e configurando variáveis de ambiente.

--- src/main/java/br/com/uanderson/springboot/domain/Anime.java
+++ src/main/java/br/com/uanderson/springboot/domain/Anime.java
@@ -1,11 +1,20 @@
 package br.com.uanderson.springboot.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Anime {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 }
