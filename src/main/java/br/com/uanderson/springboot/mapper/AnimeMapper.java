de61bd471afbf943ce2ed96cb022aa4b47448471
Uanderson Ferreira de Oliveira <uanderson.oliveira@estudante.ifto.edu.br>
2024-05-31 00:36:46 -0300

    Comentários sobre MapStruct.

--- src/main/java/br/com/uanderson/springboot/mapper/AnimeMapper.java
+++ src/main/java/br/com/uanderson/springboot/mapper/AnimeMapper.java
@@ -12,9 +12,22 @@ public abstract class AnimeMapper {
     public static final AnimeMapper INSTANCE = Mappers.getMapper(AnimeMapper.class);
     public abstract Anime toAnime(AnimePostRequestBody animePostRequestBody);
     public abstract Anime toAnime(AnimePutRequestBody animePutRequestBody);
+
 }
     /*
     MapStruct- Necessita que configuremos interfaces ou clases de mapeamento que descrevem
               como as propriedades dos objetos de origem e destino devem ser mapeadas.
 
+    @Mapping(source = "nameUser", target = "nome")
+    public abstract Anime toAnime(AnimePostRequestBody animePostRequestBody);
+
+    - Caso tivemos variaveis com nomes diferentes:
+    quando enviamos para salvar e as declaradas na nossa class, o
+    mapstruct por default preenche como 'null', para corrigir isso decemos
+    usar a anotação: @Mapping(source = "nameUser", target = "name")
+    paar que ele entenda e consiga fazer a passagem correta. Igual ao Jackson.
+    - se tiver uma regra muito especifica têm que fazer umas coisas, mais avançadas
+        olhar o vídeo abaixo paar lembrar.
+    DICA DE VÍDEO: https://youtu.be/80aMT02qAbg?si=XGrqwWMRo0RZ8xAx
+    DOCUMENTATION VERSION NOW: https://mapstruct.org/documentation/1.5/reference/html/
      */
