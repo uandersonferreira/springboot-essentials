3528e5bcb3ec614cfa2866ce8f63d642d5fc8218
Uanderson Ferreira de Oliveira <uanderson.oliveira@estudante.ifto.edu.br>
2024-06-08 18:55:33 -0300

    Spring Security CSRF Token

--- src/main/java/br/com/uanderson/springboot/config/SecurityConfig.java
+++ src/main/java/br/com/uanderson/springboot/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -14,6 +15,7 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 /**
  * Spring boot security documentation:
@@ -26,6 +28,7 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     @Bean//CONTÉM A CONFIGURAÇÃO DO QUÊ ESTAMOS PROTEGENDO COM O PROTOCOLO HTTP
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable);//desabilitando o CSRF (cenário de estudos)
         http.authorizeHttpRequests((authorize -> authorize
                         .anyRequest().authenticated())
                 )
@@ -57,3 +60,20 @@ public class SecurityConfig {
     }
 
 }//class
+/*
+O CSRF (Cross-Site Request Forgery) é um tipo de ataque que ocorre quando um
+       invasor explora a confiança de um usuário autenticado para realizar ações
+       não autorizadas em seu nome. Esse tipo de ataque geralmente envolve o envi
+       de solicitações não autorizadas de um site malicioso para um site confiável
+       no qual o usuário já está autenticado.
+
+Para prevenir ataques CSRF em um aplicativo Spring Boot, o framework fornece o suporte
+para geração e validação de tokens CSRF. O CSRF token (ou token de proteção CSRF) é um
+valor único e aleatório que é gerado e associado a cada sessão de usuário. Esse token é
+enviado ao cliente (geralmente como um cookie ou um cabeçalho HTTP) e deve ser incluído
+em todas as solicitações que alteram o estado do servidor.
+
+spring boot security doc CSRF: https://docs.spring.io/spring-security/reference/servlet/exploits/csrf.html
+
+
+ */
