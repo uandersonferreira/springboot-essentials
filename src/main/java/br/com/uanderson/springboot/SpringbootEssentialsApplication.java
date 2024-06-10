80064e95d76a37c04014456f6727e8eda912e32f
Uanderson Ferreira de Oliveira <uanderson.oliveira@estudante.ifto.edu.br>
2024-06-10 18:52:57 -0300

    Configurando a visualização da metricas no Grafana

--- src/main/java/br/com/uanderson/springboot/SpringbootEssentialsApplication.java
+++ src/main/java/br/com/uanderson/springboot/SpringbootEssentialsApplication.java
@@ -1,13 +1,23 @@
 package br.com.uanderson.springboot;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringbootEssentialsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootEssentialsApplication.class, args);
+	}
+
+	@Bean
+	MeterRegistryCustomizer<MeterRegistry> configurer(
+			@Value("${spring.application.name}") String applicationName) {
+		return (registry) -> registry.config().commonTags("application", applicationName);
 	}
 
 }
